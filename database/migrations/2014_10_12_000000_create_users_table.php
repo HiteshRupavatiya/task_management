33eace403938a556e7f11f53384c3692a48723f1
hitesh <hiteshr@zignuts.com>
2023-04-27 10:48:06 +0530

    users migration modified and added column type, deleted_at

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('type', ['Super admin', 'Admin', 'Hr', 'Team leader', 'Employee'])->default('Employee');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
